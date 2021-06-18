364ec287a4489b38371faa7f005d0c4904c80524
Kerem <kerem.comert@appcent.mobi>
2021-06-18 23:34:47 +0300

    Create fields for Repository URL construction

--- src/ProjectGUI/Constants.java
+++ src/ProjectGUI/Constants.java
@@ -5,8 +5,22 @@ public class Constants {
     public static final String BASE_URL = "http://localhost:8080/";
     public static final String GET_COMPUTER = "getComputer/?";
     public static final String GET_PHONE = "getPhone/?";
+    //endregion
+
+    //region Fields
+    public static final String MODEL = "model";
+    public static final String BRAND = "brand";
+    public static final String MIN_PRICE = "minPrice";
+    public static final String MAX_PRICE = "maxPrice";
+    public static final String SCREEN_SIZE = "screenSize";
     public static final String MIN_BATTERY_LIFE = "minBatteryLife";
     public static final String MAX_BATTERY_LIFE = "maxBatteryLife";
+    public static final String MIN_MEMORY = "minMemory";
+    public static final String MAX_MEMORY = "maxMemory";
+    public static final String SCREEN_RESOLUTION = "screenResolution";
+    public static final String PROCESSOR = "processor";
+    public static final String MIN_STORAGE_CAPACITY = "minStorageCapacity";
+    public static final String MAX_STORAGE_CAPACITY = "maxStorageCapacity";
     //endregion
 
     public static final String FXML = "GUI.fxml";
--- src/ProjectGUI/Repository.java
+++ src/ProjectGUI/Repository.java
@@ -8,27 +8,49 @@ import java.util.Arrays;
 public final class Repository {
     private static Repository instance;
 
-    private Repository(){}
+    private Repository() {
+    }
 
-    public static Repository getInstance(){
-        if(instance == null){
+    public static Repository getInstance() {
+        if (instance == null) {
             instance = new Repository();
         }
         return instance;
     }
 
-    private String appendNonNull(String fieldName, Object o){
-        return o == null ? "" : fieldName + "=" + o ;
+    private String appendNonNull(String fieldName, Object o) {
+        return o == null ? "" : fieldName + "=" + o;
     }
 
-    public void getComputers(Integer[] batteryLifeRange){
+    //These are the fields of the parent Product, that is shared by both phone and computer
+    private StringBuilder appendBaseFields(String model, String brand, Range batteryLife, String screenSize,
+                                           Range priceRange){
         StringBuilder url = new StringBuilder(Constants.BASE_URL + Constants.GET_COMPUTER);
-        url.append(appendNonNull(Constants.MIN_BATTERY_LIFE, batteryLifeRange[0]));
-        url.append(appendNonNull(Constants.MAX_BATTERY_LIFE, batteryLifeRange[1]));
+        url.append(appendNonNull(Constants.MODEL, model));
+        url.append(appendNonNull(Constants.MODEL, brand));
+        url.append(appendNonNull(Constants.MIN_BATTERY_LIFE, batteryLife.getMin()));
+        url.append(appendNonNull(Constants.MAX_BATTERY_LIFE, batteryLife.getMax()));
+        url.append(appendNonNull(Constants.SCREEN_SIZE, screenSize));
+        url.append(appendNonNull(Constants.MIN_PRICE, priceRange.getMin()));
+        url.append(appendNonNull(Constants.MAX_PRICE, priceRange.getMax()));
+        return url;
+    }
+
+    public void getComputers(String model, String brand, Range batteryLife, String screenSize,
+                             Range priceRange, String screenResolution, String processor, Range memory,
+                             Range storageCapacity) {
+        StringBuilder url = appendBaseFields(model, brand, batteryLife, screenSize, priceRange);
+        url.append(appendNonNull(Constants.SCREEN_RESOLUTION, screenResolution));
+        url.append(appendNonNull(Constants.PROCESSOR, processor));
+        url.append(appendNonNull(Constants.MIN_MEMORY, memory.getMin()));
+        url.append(appendNonNull(Constants.MAX_MEMORY, memory.getMax()));
+        url.append(appendNonNull(Constants.MIN_STORAGE_CAPACITY, storageCapacity.getMin()));
+        url.append(appendNonNull(Constants.MAX_STORAGE_CAPACITY, storageCapacity.getMax()));
         System.out.println(url.toString());
     }
 
-    public void getPhones(String filter){
-        
+    public void getPhones(String model, String brand, Range batteryLife, String screenSize,
+                          Range priceRange, Range internalMemory) {
+        StringBuilder url = appendBaseFields(model, brand, batteryLife, screenSize, priceRange);
     }
 }
